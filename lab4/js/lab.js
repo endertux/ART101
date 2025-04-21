ed99d577093c652eaf095c394611dac2ed26c5f3
Helwa <44377460+endertux@users.noreply.github.com>
2025-04-21 15:25:36 -0700

    Created Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,46 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Sticks
+// An implementation of the game
+// Helwa Halloum <hhalloum@ucsc.edu>
+// April 2025
 
 // Constants
 
 // Functions
+
+// Helwa's Simple Computer Game Pseudocode
+// Sticks (finger game)
+
+	// Title Screen
+						  //Accept user input to start
+						// Await user input for players (min: 2)
+						  //Accept user input (min players: 2)
+						// Game start
+						  // Accept player 1 move
+						  // Validate player 1 move
+						  // Evaluate possible moves from player 1
+						  // Accept player 2 move
+						  // Validate player 2 move
+						  // Evaluate possible moves from player 2
+						  // Test of win or draw condition
+							// if player hand reaches to count: 5
+							// then hand loses
+
+
+
+// Running Monster Hunter on my laptop
+
+						// Startup Gaming hub
+						// Prompt user to switch graphic driver
+						// Accept user input
+						// Wait until laptop reboots
+						// Prompt user to log in laptop
+						// Accept input
+						// Prompt user MSI Afterburner
+						// User accepts MSI Afterburner
+						// User input to open Monster Hunter
+						// Accept user input
+						// User waits until the game starts up
+						// Game start
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
