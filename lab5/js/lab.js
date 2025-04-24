fa6c439a4a119900c5dad5278798c5ee3f25cecc
Helwa <44377460+endertux@users.noreply.github.com>
2025-04-24 16:10:06 -0700

    KMS

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -15,6 +15,14 @@
   // calculate
   age = 2019 - year;
 
+  //output
+  document.writeLn("Make: " + make + "<br>");
+  document.writeLn("Model: " + model + "<br>");
+  document.writeLn("Color: " + color + "<br>");
+  document.writeLn("Year: " + year + "<br>");
+  document.writeLn("Age: " + age + "<br>");
+
+  
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
@@ -24,14 +32,7 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
-  
-  
-  //output
-  document.writeLn("Make: " + make + "<br>");
-  document.writeLn("Model: " + model + "<br>");
-  document.writeLn("Color: " + color + "<br>");
-  document.writeLn("Year: " + year + "<br>");
-  document.writeLn("Age: " + age + "<br>");
+
 }
 
 // let's get this party started
