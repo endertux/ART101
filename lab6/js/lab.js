9edce1066448debb2a895e712a96611532eb2369
Helwa <44377460+endertux@users.noreply.github.com>
2025-04-28 17:31:24 -0700

    created lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,36 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Helwa Halloum
+// Date: 4/28/25
 
 // Constants
 
 // Functions
+
+///////////////////////////////////
+
+// Define var
+myTransport = ["Foot", "Jeep", "Rides from friends", "Uber/Lyft"];
+
+// create object of main ride
+var myMainRide = {
+  make: "Jeep",
+  model: "Safari",
+  color: "Black",
+  year: 2022,
+  age: function() {
+      return 2025 - this.year;
+  }
+}
+
+//output
+document.writeln("Getting around" + myTransport + "<br>");
+
+//print
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
+///////////////////////////////////
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
