5b95fcdd003dc4d45cbe7afd18328fa13cac7a67
Helwa <44377460+endertux@users.noreply.github.com>
2025-05-02 15:04:57 -0700

    fixed bug

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -9,7 +9,7 @@
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function shuffleUserName () {
   // some code here
-  var userName = window.prompt("Want to see something cool? Type your name.");
+  const userName = window.prompt("Want to see something cool? Type your name.");
   console.log("userName =", userName);
 
   var nameArray = userName.split('');
@@ -26,4 +26,4 @@ function shuffleUserName () {
 
 // output
 document.writeln("Now this is epic: ", 
-  sortUserName(), "</br>");
+  shuffleUserName(), "</br>");
