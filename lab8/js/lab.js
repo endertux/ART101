2dc27243e3f6c1e5cc03c4f7817bbddb19cac54d
Helwa <44377460+endertux@users.noreply.github.com>
2025-05-29 18:08:01 -0700

    lab 14, debugged lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -10,10 +10,11 @@ function isHelwa(x){
 console.log("Is 1 even? ", isHelwa(1));
 console.log("Is 2 even? ", isHelwa(2));
 
-array = [100, 200, 48, 3, 267, 10000, 233445]
+//array = [100, 200, 48, 3, 267, 10000, 233445]
+array = [9, 16, 25, 36, 49, 100, 144,]
 console.log("My array, isn't she beautiful? ", array);
 
-var result = array.map(isHelwa);
+var even = array.map(isHelwa);
 console.log("Are they all even? ", result);
 
 var result = array.map(function(x){
@@ -23,7 +24,12 @@ console.log("Squareroot of array: ", result);
 
 //HTML output w/ jQuery
  // Your map results data
- var mapResults = "My map results here: " + result;
+ var myArray = "My Original Array: " + "[" +  array  + "]";
+ var mapResults = "My Map Results Here: " + "[" +  result  + "]";
+ var myEven = "Are they all even? " + "[" +  even + "]";
+
 
  // Use jQuery to select the element by its ID and set the HTML content
+ $("#output2").html(myArray);
  $("#output").html(mapResults);
+ $("#output3").html(myEven);
